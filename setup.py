9c6afcb963443b59fc51b391d0ce64c4f1cc49a2
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2021-05-19 11:23:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: Pre-Alpha",
+        "Development Status :: 2 - Pre-Alpha",
         "Topic :: Communications :: Chat",
         "Topic :: Utilities",
         "Intended Audience :: Developers",
