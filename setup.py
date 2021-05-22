24764595a93b3f3132de1865864c2f05d77b621f
Knugi <24708955+KnugiHK@users.noreply.github.com>
2021-05-22 06:46:43 +0000

    Remove classifier

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.8",
