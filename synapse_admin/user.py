4e5a0b5b80662efea49ab88d668983d380ec20c9
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2021-06-12 17:31:37 +0800

    Add docstring for User and _Device classes

--- synapse_admin/user.py
+++ synapse_admin/user.py
@@ -61,14 +61,30 @@ class User(Admin):
 
     def lists(
         self,
-        offset: int = 0,
+        offset: Union[int, str] = 0,
         limit: int = 100,
         userid: str = None,
         name: str = None,
         guests: bool = True,
         deactivated: bool = False,
         order_by: str = None
-    ) -> Tuple[str, int]:
+    ) -> Tuple[list, int]:
+        """List all local users
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#list-accounts
+
+        Args:
+            offset (Union[int, str], optional): equivalent to "from". Defaults to 0. # noqa: E501
+            limit (int, optional): equivalent to "limit". Defaults to 100.
+            userid (str, optional): equivalent to "user_id". Defaults to None.
+            name (str, optional): equivalent to "name". Defaults to None.
+            guests (bool, optional): equivalent to "guests". Defaults to True.
+            deactivated (bool, optional): equivalent to "deactivated". Defaults to False. # noqa: E501
+            order_by (str, optional): equivalent to "order_by". Defaults to None.
+
+        Returns:
+            Tuple[list, int]: list of user, total number of returned users
+        """
         optional_str = ""
         if userid is not None:
             userid = self.validate_username(userid)
@@ -99,10 +115,12 @@ class User(Admin):
     def create(self, userid: str, **kwargs) -> bool:
         """Create or modify a user
 
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#create-or-modify-account
+
         Args:
             userid (str): The user id of the user
 
-        Extra Args (use by specifying the argument name):
+        Extra Args (use by specifying the argument name) (all extra args are equivalent to the Synapse docs): # noqa: E501
             password (str)
             displayname (str)
             threepids (list)
@@ -128,7 +146,17 @@ class User(Admin):
             else:
                 raise SynapseException(data["errcode"], data["error"])
 
-    def query(self, userid: str) -> Union[list, dict]:
+    def query(self, userid: str) -> dict:
+        """Query a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#query-user-account
+
+        Args:
+            userid (str): the user you want to query
+
+        Returns:
+            dict: account information
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -137,6 +165,16 @@ class User(Admin):
         return resp.json()
 
     def active_sessions(self, userid: str) -> list:
+        """Query a user for their current sessions
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#query-current-sessions-for-a-user
+
+        Args:
+            userid (str): the user you want to query
+
+        Returns:
+            list: list of sessions
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -146,6 +184,17 @@ class User(Admin):
         return data["sessions"][0]["connections"]
 
     def deactivate(self, userid: str, erase: bool = True) -> bool:
+        """Deactivate a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#deactivate-account
+
+        Args:
+            userid (str): the account you want to deactivate
+            erase (bool, optional): whether to erase all information related to the user. Defaults to True. # noqa: E501
+
+        Returns:
+            bool: success or not
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "POST",
@@ -155,6 +204,15 @@ class User(Admin):
         return resp.json()["id_server_unbind_result"] == "success"
 
     def reactivate(self, userid: str, password: str = None) -> bool:
+        """Reactivate a deactivated account
+
+        Args:
+            userid (str): the account you want to reactivate
+            password (str, optional): a new password for the account. Defaults to None. # noqa: E501
+
+        Returns:
+            bool: success or not
+        """
         if password is None:
             password = Utility.get_password()
         if not isinstance(password, str):
@@ -170,6 +228,18 @@ class User(Admin):
         password: str = None,
         logout: bool = True
     ) -> bool:
+        """Reset a user password
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#reset-password
+
+        Args:
+            userid (str): the account you want to reset their password
+            password (str, optional): the new password. Defaults to None.
+            logout (bool, optional): whether or not to logout all current devices. Defaults to True. # noqa: E501
+
+        Returns:
+            bool: success or not
+        """
         userid = self.validate_username(userid)
         if password is None:
             password = Utility.get_password()
@@ -188,6 +258,16 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def is_admin(self, userid: str) -> bool:
+        """To see if a user is a server admin
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#get-whether-a-user-is-a-server-administrator-or-not
+
+        Args:
+            userid (str): the user you want to query
+
+        Returns:
+            bool: is or is not an admin
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -196,6 +276,17 @@ class User(Admin):
         return resp.json()["admin"]
 
     def set_admin(self, userid: str, activate: bool = None) -> bool:
+        """Set or revoke server admin role for a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#change-whether-a-user-is-a-server-administrator-or-not
+
+        Args:
+            userid (str): the user you want to set or revoke
+            activate (bool, optional): True to set as admin, False to revoke their admin, leave None to let the program decide. Defaults to None. # noqa: E501
+
+        Returns:
+            bool: success or not
+        """
         if activate is None:
             activate = not self.is_admin(userid)
         elif not isinstance(activate, bool):
@@ -219,6 +310,16 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def joined_room(self, userid: str) -> Tuple[list, int]:
+        """Query the room a user joined
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#list-room-memberships-of-a-user
+
+        Args:
+            userid (str): the user you want to query
+
+        Returns:
+            Tuple[list, int]: list of joined_room, total number of joined room
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -234,6 +335,17 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def join_room(self, userid: str, room: str) -> bool:
+        """Force an user to join a room
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/room_membership.md#edit-room-membership-api
+
+        Args:
+            userid (str): the user you want to add to the room
+            room (str): the room you want to add the user into
+
+        Returns:
+            bool: success or not
+        """
         userid = self.validate_username(userid)
         room = self.validate_room(room)
         resp = self.connection.request(
@@ -257,6 +369,18 @@ class User(Admin):
         expiration: int = 0,
         enable_renewal_emails: bool = True
     ) -> dict:
+        """Set an account validity
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/account_validity.md#account-validity-api
+
+        Args:
+            userid (str): the user you want to set
+            expiration (int, optional): target expiration timestamp in millisecond. Defaults to 0. # noqa: E501
+            enable_renewal_emails (bool, optional): enable or disable the renewal email. Defaults to True. # noqa: E501
+
+        Returns:
+            dict: the new expiration timestamp in millisecond
+        """
         if expiration is not None and not isinstance(expiration, int):
             raise TypeError(
                 "Argument 'expiration' only accept "
@@ -293,6 +417,20 @@ class User(Admin):
         password: str = None,
         admin: bool = False
     ) -> dict:
+        """Register a new user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/register_api.md#shared-secret-registration
+
+        Args:
+            username (str): the username
+            shared_secret (Union[str, bytes]): the shared secret defined in homeserver.yaml # noqa: E501
+            displayname (str): the display name for the user
+            password (str, optional): the password for the user. Defaults to None.
+            admin (bool, optional): whether or not to set the user as server admin. Defaults to False. # noqa: E501
+
+        Returns:
+            dict: a dict including access token and other information of the new account
+        """
         nonce = self._get_register_nonce()
         if password is None:
             password = Utility.get_password()
@@ -313,6 +451,11 @@ class User(Admin):
         return resp.json()
 
     def _get_register_nonce(self) -> str:
+        """Get a register nonce
+
+        Returns:
+            str: register nonce
+        """
         resp = self.connection.request(
             "GET",
             self.admin_patterns("/register", 1)
@@ -328,9 +471,12 @@ class User(Admin):
         admin=False,
         user_type=None
     ) -> str:
-        """
-        Adapted from:
-        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/register_api.rst
+        """Generate a HMAC for register
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/register_api.md#shared-secret-registration
+
+        Returns:
+            str: HMAC-SHA1 value
         """
         if isinstance(shared_secret, str):
             shared_secret = shared_secret.encode()
@@ -363,6 +509,20 @@ class User(Admin):
         order_by: int = None,
         _dir: str = "f"
     ) -> Tuple[list, int, int]:
+        """list all media sent by the user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#list-media-of-a-user
+
+        Args:
+            userid (str): the user you want to query
+            limit (int, optional): equivalent to "limit". Defaults to 100.
+            _from (int, optional): equivalent to "from". Defaults to 0.
+            order_by (int, optional): equivalent to "order_by". Defaults to None. # noqa: E501
+            _dir (str, optional): equivalent to "dir". Defaults to "f".
+
+        Returns:
+            Tuple[list, int, int]: list of media, next token, total number of media # noqa: E501
+        """
         userid = self.validate_username(userid)
         optional_str = ""
         if order_by is not None:
@@ -388,6 +548,17 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def login(self, userid: str, valid_until_ms: int = None) -> str:
+        """Login as a user and get their access token
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#login-as-a-user
+
+        Args:
+            userid (str): the user you want to login
+            valid_until_ms (int, optional): the validity period in millisecond. Defaults to None. # noqa: E501
+
+        Returns:
+            str: access token of the user
+        """
         if isinstance(valid_until_ms, int):
             data = {"valid_until_ms": valid_until_ms}
         elif valid_until_ms is None:
@@ -414,6 +585,16 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def get_ratelimit(self, userid: str) -> Tuple[int, int]:
+        """Query the ratelimit applied to a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#get-status-of-ratelimit
+
+        Args:
+            userid (str): the user you want to query
+
+        Returns:
+            Tuple[int, int]: the current messages per second and burst count
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -435,6 +616,18 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def set_ratelimit(self, userid: str, mps: int, bc: int) -> Tuple[int, int]:
+        """Set the ratelimit applied to a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#set-ratelimit
+
+        Args:
+            userid (str): the user you want to set
+            mps (int): messages per second
+            bc (int): burst count
+
+        Returns:
+            Tuple[int, int]: the current messages per second and burst count
+        """
         userid = self.validate_username(userid)
         data = {"messages_per_second": mps, "burst_count": bc}
         resp = self.connection.request(
@@ -456,9 +649,27 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def disable_ratelimit(self, userid: str) -> Tuple[int, int]:
+        """Disable the ratelimit applied to a user
+
+        Args:
+            userid (str): the user you want to disable their ratelimit
+
+        Returns:
+            Tuple[int, int]: the current messages per second and burst count
+        """
         return self.set_ratelimit(userid, 0, 0)
 
     def delete_ratelimit(self, userid: str) -> bool:
+        """Delete the ratelimit applied to a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#delete-ratelimit
+
+        Args:
+            userid (str): the user you want to delete their ratelimit
+
+        Returns:
+            bool: success or not
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "DELETE",
@@ -478,6 +689,16 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def pushers(self, userid: str) -> Tuple[list, int]:
+        """list pushers of a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#list-all-pushers
+
+        Args:
+            userid (str): the user you want to query
+
+        Returns:
+            Tuple[list, int]: list of pushers, total number of pushers
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -494,6 +715,16 @@ class User(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def shadow_ban(self, userid: str) -> bool:
+        """Shadow ban a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#shadow-banning-users
+
+        Args:
+            userid (str): the user you want to shadow ban
+
+        Returns:
+            bool: success or not
+        """
         print("WARNING! This action may Undermine the TRUST of YOUR USERS.")
         userid = self.validate_username(userid)
         resp = self.connection.request(
@@ -516,6 +747,16 @@ class _Device(Admin):
         self.connection = conn
 
     def lists(self, userid: str) -> list:
+        """List all active devices of a user
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#list-all-devices
+
+        Args:
+            userid (str): the user you want to query
+
+        Returns:
+            list: list of active devices
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -531,6 +772,17 @@ class _Device(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def delete(self, userid: str, device: Union[str, list]) -> bool:
+        """Delete active device(s)
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#delete-multiple-devices
+
+        Args:
+            userid (str): the owner of the device(s)
+            device (Union[str, list]): the device(s) you want to delete
+
+        Returns:
+            bool: success or not
+        """
         if isinstance(device, list) and len(device) > 1:
             return self._delete_multiple(userid, device)
         elif isinstance(device, list) and len(device) == 1:
@@ -547,6 +799,17 @@ class _Device(Admin):
             return False
 
     def _delete_multiple(self, userid: str, devices: list) -> bool:
+        """Delete multiple active devices (You should use User.delete)
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#delete-multiple-devices
+
+        Args:
+            userid (str): the owner of the device(s)
+            device (list): the device(s) you want to delete
+
+        Returns:
+            bool: success or not
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "POST",
@@ -559,6 +822,17 @@ class _Device(Admin):
             return False
 
     def show(self, userid: str, device: str) -> dict:
+        """Show details of a device
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#show-a-device
+
+        Args:
+            userid (str): the owner of the device
+            device (str): the device you want to query
+
+        Returns:
+            dict: dict including last seen IP address and other information
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "GET",
@@ -574,6 +848,18 @@ class _Device(Admin):
                 raise SynapseException(data["errcode"], data["error"])
 
     def update(self, userid: str, device: str, display_name: str) -> bool:
+        """Update the display name of a device
+
+        https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/user_admin_api.md#update-a-device
+
+        Args:
+            userid (str): the owner of the device
+            device (str): the device you want to modify
+            display_name (str): the new display name
+
+        Returns:
+            bool: success or not
+        """
         userid = self.validate_username(userid)
         resp = self.connection.request(
             "PUT",
