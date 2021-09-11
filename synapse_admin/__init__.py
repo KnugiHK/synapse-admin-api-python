2f3a81ab027f0145a390bf7a2aa9f8e9a395181c
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2021-09-11 14:02:22 +0800

    Bump version

--- synapse_admin/__init__.py
+++ synapse_admin/__init__.py
@@ -3,7 +3,7 @@ if sys.version_info < (3, 7):
     print("matrix-synapse-admin requires Python 3.7 or above.")
     sys.exit(1)
 
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 
 from synapse_admin.user import User  # noqa: F401
 from synapse_admin.management import Management  # noqa: F401
